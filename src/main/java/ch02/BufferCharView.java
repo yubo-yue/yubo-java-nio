8486b1ea72159dc58cb14a1c595c3c7ab29379ff
yubo <yubo@CN_PEK_104507.ant.amazon.com>
2013-09-17 15:40:24 +0800

    ByteBuffer test

---
+++ src/main/java/ch02/BufferCharView.java
@@ -0,0 +1,36 @@
+package ch02;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.CharBuffer;
+
+public class BufferCharView {
+
+	private static void println(Buffer buffer) {
+		System.out.println("pos=" + buffer.position() + ", limit="
+				+ buffer.limit() + ", capacity=" + buffer.capacity() + " : "
+				+ buffer.toString());
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ByteBuffer byteBuffer = ByteBuffer.allocate(7).order(ByteOrder.BIG_ENDIAN);
+		
+		CharBuffer charBuffer = byteBuffer.asCharBuffer();
+		
+		byteBuffer.put(0, (byte)0);
+		byteBuffer.put(1, (byte)'H');
+		byteBuffer.put(2, (byte)0);
+		byteBuffer.put(3, (byte)'i');
+		byteBuffer.put(4, (byte)0);
+		byteBuffer.put(5, (byte)'!');
+		byteBuffer.put(6, (byte)0);
+		
+		println(byteBuffer);
+		println(charBuffer);
+	}
+
+}
---
+++ src/main/java/ch02/BufferFillDrain.java
@@ -0,0 +1,45 @@
+package ch02;
+
+import java.nio.CharBuffer;
+
+public class BufferFillDrain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CharBuffer buffer = CharBuffer.allocate(100);
+		
+		while (fillBuffer(buffer))
+		{
+			buffer.flip();
+			drainBuffer(buffer);
+			buffer.clear();
+		}
+	}
+
+	private static void drainBuffer(CharBuffer buffer) {
+		while (buffer.hasRemaining()) {
+			System.out.print(buffer.get());
+		}
+		System.out.println("");
+	}
+
+	private static boolean fillBuffer(final CharBuffer buffer) {
+		if (index >= strings.length)
+			return false;
+		String string = strings[index++];
+		for (int i = 0; i < string.length(); i++)
+			buffer.put(string.charAt(i));
+		
+		return true;
+	}
+
+	private static int index = 0;
+	private static String[] strings = { "A random string value",
+			"The product of an infinite number of monkeys",
+			"Hey hey we're the Monkees",
+			"Opening act for the Monkees: Jimi Hendrix",
+			"'Scuse me while I kiss this fly", // Sorry Jimi ;-)
+			"Help Me! Help Me!", };
+}
