c155dd61cddea6f56ba49c2078643354e1dc7626
yubo <yubo.yue@163.com>
2015-04-21 09:43:01 +0800

    jnp ch04

---
+++ src/main/java/org/simple/ch04/NetworkInterfaceUtility.java
@@ -0,0 +1,10 @@
+package org.simple.ch04;
+
+public class NetworkInterfaceUtility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/org/simple/ch04/OReillyByName.java
@@ -0,0 +1,39 @@
+package org.simple.ch04;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OReillyByName {
+	private static final Logger LOGGER = LoggerFactory.getLogger(OReillyByName.class);
+
+	public static void main(String[] args) throws UnknownHostException {
+		
+		OReillyByName.getInetAddressByHostname();
+		OReillyByName.getInetAddressOfLocal();
+	}
+	
+	public static void getInetAddressByHostname() throws UnknownHostException
+	{
+		String hostName = "www.oreilly.com";
+		InetAddress address = InetAddress.getByName(hostName);
+		LOGGER.info("{}", address);
+		InetAddress[] allAddresses = InetAddress.getAllByName(hostName);
+		for (InetAddress ia : allAddresses) {
+			LOGGER.info("{}", ia);
+		}
+	}
+	
+	public static void getInetAddressOfLocal()
+	{
+		try {
+			InetAddress address = InetAddress.getLocalHost();
+			LOGGER.info("{}", address);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
